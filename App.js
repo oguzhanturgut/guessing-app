89463b7f424acf4b993e29794d09df6a290b97f2
Oguzhan <oguzhanturgut@outlook.com>
2020-03-18 01:41:20 +0100

    base game screen

---
+++ App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+import Header from "./components/Header";
+import StartGameScreen from "./screens/StartGameScreen";
+
+export default function App() {
+  return (
+    <View style={styles.screen}>
+      <Header title="Guess a Number" />
+      <StartGameScreen/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1
+  },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
